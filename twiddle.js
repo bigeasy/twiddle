8419661a5235c7e095b682d2cc45ab98b5607814
Alan Gutierrez <alan@prettyrobots.com>
2020-10-06 00:18:22 -0500

    Rename a varaible.
    See #50.

--- twiddle.js
+++ twiddle.js
@@ -1,8 +1,8 @@
-module.exports = function (inner, twiddle) {
+module.exports = function (untwiddled, twiddle) {
     const iterator = {
         done: false,
         next (promises, consume, terminator = iterator) {
-            inner.next(promises, items => {
+            untwiddled.next(promises, items => {
                 consume(twiddle(items))
             }, terminator)
         }
